f6ef03a1ca9445489fc487ad4456dd7e3a9001ac
Vitaliy FutureDeveloper <zarvoron@yandex.ru>
2020-07-02 21:01:59 +0500

    Начало вёрстки. Создал структуру папок проекта

---
+++ gulp/tasks/pug2html.js
@@ -0,0 +1,14 @@
+//const  = require('');
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const plumber = require('gulp-plumber');
+const htmlValidator = require('gulp-w3c-html-validator');
+const bemValidator = require('gulp-html-bem-validator');
+
+module.exports = function pug2html(cb) {
+	return gulp.src('src/pages/*pug')
+	.pipe(plumber())
+	.pipe(pug())
+	.pipe(htmlValidator())
+	.pipe(gulp.dest('build'))
+}
